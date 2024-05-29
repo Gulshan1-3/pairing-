17ef37d6488a4e3ca271a87956ed5d4999ce761b
Gulshan1-3 <ankitstar955@gmail.com>
2024-05-29 11:27:34 +0530

    few errors left to solve

--- pair/src/main.rs
+++ pair/src/main.rs
@@ -1,3 +1,40 @@
+extern crate bn;
+extern crate rand_core;
+extern crate rand;
+
+use bn::{pairing, Fr, G1, G2};
+use crate::bn::Group;
+
+use rand_core::OsRng;
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
-}
+    let rng = &mut rand::thread_rng();
+
+    let a = Fr::random(rng);
+    let b = Fr::random(rng);
+    let c = Fr::random(rng);
+
+
+    let (pa,qa) = (G1::one()*a,G2::one()*a);
+    let (pb,qb) = (G1::one()*b,G2::one()*b);
+    let (pc,qc) = (G1::one()*c,G2::one()*c);
+
+
+    let alice_ss = pairing(pb,qc).pow(a);
+    let bob_ss = pairing(pb, qb).pow(b);
+    let ron_ss = pairing(pc, qc).pow(c);
+
+
+    if alice_ss == bob_ss && bob_ss == ron_ss {
+        println!("\nSharing has worked!");
+    }
+    else {
+       println!("\nSharing has failed!");
+    }
+   }
+    
+
--- pair/target/debug/deps/pair-5256928c85fab978.d
+++
@@ -1,5 +0,0 @@
-/home/gulshansharma/pairing-/pair/target/debug/deps/libpair-5256928c85fab978.rmeta: src/main.rs
-
-/home/gulshansharma/pairing-/pair/target/debug/deps/pair-5256928c85fab978.d: src/main.rs
-
-src/main.rs:
